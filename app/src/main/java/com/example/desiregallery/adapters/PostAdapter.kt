81d53832b803430c944710c4afa95c649009588a
Konstantin <k_babaec@mail.ru>
2019-03-10 21:05:31 +0500

    Refactoring

--- app/src/main/java/com/example/desiregallery/adapters/PostAdapter.kt
+++ app/src/main/java/com/example/desiregallery/adapters/PostAdapter.kt
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.desiregallery.R
 import com.example.desiregallery.models.Post
+import com.example.desiregallery.ui.widgets.SquareImageView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_post.view.*
 
@@ -16,23 +18,23 @@ class PostAdapter(val items : List<Post>, val context: Context) : RecyclerView.A
         return items.size
     }
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_post, p0, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_post, parent, false))
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        val item : Post = items.get(p1)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item : Post = items[position]
         Picasso.with(context)
             .load(item.getImageUrl().toString())
             .placeholder(R.drawable.image_placeholder)
             .error(R.drawable.image_error)
-            .into(p0.imageView)
-        p0.ratingTextView.text = context.getString(R.string.rating_text, item.getRating())
+            .into(holder.imageView)
+        holder.ratingTextView.text = context.getString(R.string.rating_text, item.getRating())
 
     }
 
     class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-        val imageView = view.item_post_image
-        val ratingTextView = view.item_post_rating
+        val imageView : SquareImageView = view.item_post_image
+        val ratingTextView : TextView = view.item_post_rating
     }
 }
