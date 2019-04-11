4b5ae3b16abe8a876bd52824fb2a12aa04d0612b
babaetskv <babaetskv@intersvyaz.net>
2019-04-11 15:10:10 +0500

    Adding options menu on the image full screen

--- app/src/main/java/com/example/desiregallery/ui/activities/FullScreenImageActivity.kt
+++ app/src/main/java/com/example/desiregallery/ui/activities/FullScreenImageActivity.kt
@@ -4,8 +4,12 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.desiregallery.R
 import com.example.desiregallery.Utils
 import kotlinx.android.synthetic.main.activity_full_screen_image.*
@@ -16,6 +20,8 @@ class FullScreenImageActivity : AppCompatActivity() {
         const val EXTRA_IMAGE = "bytesImage"
     }
 
+    private lateinit var toolbar: Toolbar
+
     private lateinit var image: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +31,33 @@ class FullScreenImageActivity : AppCompatActivity() {
         else
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_full_screen_image)
+
+        toolbar = findViewById(R.id.image_screen_toolbar)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+
         image = Utils.bytesToBitmap(intent.getByteArrayExtra(EXTRA_IMAGE))
         image_view_full_screen.setImageBitmap(image)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_image_screen, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            R.id.image_download -> {
+                // TODO
+                Toast.makeText(this, "Download item have been clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.image_share -> {
+                // TODO
+                Toast.makeText(this, "Share item have been clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
+        }
     }
 }
