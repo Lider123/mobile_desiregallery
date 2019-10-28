030dc7d21b3b7f732985cf50e830a4265ef59e05
babaetskv <babaetskv@intersvyaz.net>
2019-10-28 11:05:04 +0500

    Added about window

---
+++ app/src/main/java/com/example/desiregallery/ui/dialogs/AboutDialog.kt
@@ -0,0 +1,32 @@
+package com.example.desiregallery.ui.dialogs
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.util.Linkify
+import android.widget.TextView
+import com.example.desiregallery.R
+import com.example.desiregallery.utils.getAppVersion
+
+/**
+ * @author babaetskv on 28.10.19
+ */
+class AboutDialog(context: Context) : AlertDialog(context) {
+
+    init {
+        val message = TextView(context)
+        message.setPadding(60, 40, 40, 40)
+        val s = SpannableString(context.getString(R.string.about_message, getAppVersion(context)))
+        Linkify.addLinks(s, Linkify.WEB_URLS)
+        message.text = s
+        message.movementMethod = LinkMovementMethod.getInstance()
+
+        setTitle(R.string.app_name)
+        setCancelable(true)
+        setIcon(R.drawable.logo_icon)
+        setButton(DialogInterface.BUTTON_POSITIVE, context.getString(R.string.OK)) { dialog, _ -> dialog.dismiss() }
+        setView(message)
+    }
+}
